9f707213ea16c9625c4092b6fcf8fb6d12ebdcad
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2022-09-12 18:56:21 -0300

    Improve error handling also remove unnecessary tests

--- deltacoverage.go
+++ deltacoverage.go
@@ -3,12 +3,17 @@ package deltacoverage
 import (
 	"bufio"
 	"fmt"
+	"io/fs"
 	"os"
-	"path"
+	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
+
+	"gopkg.in/errgo.v2/errors"
 )
+
+var ErrMustBeDirectory = errors.New("Must be a directory")
 
 type CoverProfile struct {
 	TotalStatements int
@@ -29,55 +34,67 @@ func (c CoverProfile) String() string {
 	return strings.Join(output, "\n")
 }
 
-func ParseCoverProfile(coverFilePath string, extraFilePaths ...string) (CoverProfile, error) {
+func ParseCoverProfile(dirPath string) (CoverProfile, error) {
+	info, err := os.Stat(dirPath)
+	if err != nil {
+		return CoverProfile{}, err
+	}
+	if !info.IsDir() {
+		return CoverProfile{}, ErrMustBeDirectory
+	}
 	covProfile := CoverProfile{
 		Branches: map[string]int{},
 		Tests:    map[string][]string{},
 	}
-	allCoverFilePaths := append([]string{coverFilePath}, extraFilePaths...)
-	for _, coverPath := range allCoverFilePaths {
-		testName := strings.Split(path.Base(coverPath), ".")[0]
-		f, err := os.Open(coverPath)
+	err = filepath.Walk(dirPath, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
-			return CoverProfile{}, err
+			return err
 		}
-		defer f.Close()
-		scanner := bufio.NewScanner(f)
-		// dicard first line `mode: xxx`
-		scanner.Scan()
-		// line example with headers
-		// identifier          statements visited
-		// xyz/xyz.go:3.24,5.2 1          1
-		for scanner.Scan() {
-			items := strings.Fields(scanner.Text())
-			nrStmt, err := strconv.Atoi(items[1])
+		matched, err := filepath.Match("*.coverprofile", filepath.Base(path))
+		if err != nil {
+			return err
+		}
+		if matched {
+			testName := strings.Split(filepath.Base(path), ".")[0]
+			f, err := os.Open(path)
 			if err != nil {
-				return CoverProfile{}, err
+				return err
 			}
-			_, exists := covProfile.Branches[items[0]]
-			if !exists {
-				covProfile.Branches[items[0]] = nrStmt
-				covProfile.TotalStatements += nrStmt
-				covProfile.Tests[testName] = append(covProfile.Tests[testName], items[0])
-				continue
-			}
-			// remove references from other tests
-			ids := []string{}
-			for testName, tests := range covProfile.Tests {
-				for _, id := range tests {
-					if id == items[0] {
-						continue
-					}
-					ids = append(ids, id)
+			defer f.Close()
+			scanner := bufio.NewScanner(f)
+			// dicard first line `mode: xxx`
+			scanner.Scan()
+			// line example with headers
+			// identifier          statements visited
+			// xyz/xyz.go:3.24,5.2 1          1
+			for scanner.Scan() {
+				items := strings.Fields(scanner.Text())
+				nrStmt, err := strconv.Atoi(items[1])
+				if err != nil {
+					return err
 				}
-				covProfile.Tests[testName] = ids
+				_, exists := covProfile.Branches[items[0]]
+				if !exists {
+					covProfile.Branches[items[0]] = nrStmt
+					covProfile.TotalStatements += nrStmt
+					covProfile.Tests[testName] = append(covProfile.Tests[testName], items[0])
+					continue
+				}
+				// insert empty result
+				covProfile.Tests[testName] = []string{}
+				// remove references from other tests
+				for testName := range covProfile.Tests {
+					covProfile.Tests[testName] = []string{}
+				}
 			}
-			// insert empty result
-			covProfile.Tests[testName] = []string{}
+			if err := scanner.Err(); err != nil {
+				return err
+			}
 		}
-		if err := scanner.Err(); err != nil {
-			return CoverProfile{}, err
-		}
+		return nil
+	})
+	if err != nil {
+		return CoverProfile{}, err
 	}
 	return covProfile, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/broadwing/deltacoverage
 
 go 1.19
 
-require github.com/google/go-cmp v0.5.8
+require (
+	github.com/google/go-cmp v0.5.8
+	gopkg.in/errgo.v2 v2.1.0
+)
