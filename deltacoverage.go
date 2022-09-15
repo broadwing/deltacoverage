622f331d16ac91b8fbc099701037273ae0d017d6
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2022-09-15 11:37:00 -0300

    Sum the total statements in the ParseCoverProfile function

--- deltacoverage.go
+++ deltacoverage.go
@@ -16,6 +16,12 @@ import (
 var ErrMustBeDirectory = errors.New("Must be a directory")
 var ErrMustBeFile = errors.New("Must be a file")
 
+type ProfileItem struct {
+	Branch     string
+	Statements int
+	Visited    bool
+}
+
 type CoverProfile struct {
 	DirPath          string
 	NumberStatements int
@@ -23,6 +29,35 @@ type CoverProfile struct {
 	Tests            map[string][]string
 }
 
+// ParseProfileLine returns a pointer to ProfileItem type for a given string.
+// line example with headers
+// identifier          statements visited
+// xyz/xyz.go:3.24,5.2 1          1
+func (c CoverProfile) ParseProfileLine(line string) (*ProfileItem, error) {
+	if strings.HasPrefix(line, "mode:") {
+		return &ProfileItem{}, nil
+	}
+	items := strings.Fields(line)
+	branch := items[0]
+	profItem := &ProfileItem{
+		Branch: branch,
+	}
+	nrStmt, err := strconv.Atoi(items[1])
+	if err != nil {
+		return &ProfileItem{}, err
+	}
+	profItem.Statements = nrStmt
+	timesVisited, err := strconv.Atoi(items[2])
+	if err != nil {
+		return &ProfileItem{}, err
+	}
+	if timesVisited > 0 {
+		profItem.Visited = true
+	}
+	return profItem, err
+}
+
+// String prints out the deltacoverage percentage for each test
 func (c CoverProfile) String() string {
 	output := []string{}
 	for testName, ids := range c.Tests {
@@ -40,14 +75,24 @@ func (c CoverProfile) String() string {
 	return strings.Join(output, "\n")
 }
 
+// ParseCoverProfile reads all files from a directory with extension
+// .coverprofile, parses it, and populate CoverProfile object
+// Design tradeoff to get the total number of statements:
+// 1. Parse one file just to sum the total statements
+// 2. Recalculate total statements each iteration on the loop
+// 3. Add a counter and an if to just calculate the total sum in the first
+// iteration
+// Current implementation in number 3
 func (c *CoverProfile) ParseCoverProfile() error {
 	branchesCount := map[string]int{}
 	branchesStmts := map[string]int{}
+	profilesRead := 0
 	err := filepath.Walk(c.DirPath, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if filepath.Ext(info.Name()) == ".coverprofile" {
+			profilesRead++
 			testName := strings.Split(filepath.Base(path), ".")[0]
 			f, err := os.Open(path)
 			if err != nil {
@@ -55,31 +100,25 @@ func (c *CoverProfile) ParseCoverProfile() error {
 			}
 			defer f.Close()
 			scanner := bufio.NewScanner(f)
-			// dicard first line `mode: xxx`
-			scanner.Scan()
-			// line example with headers
-			// identifier          statements visited
-			// xyz/xyz.go:3.24,5.2 1          1
 			for scanner.Scan() {
-				items := strings.Fields(scanner.Text())
-				branch := items[0]
-				nrStmt, err := strconv.Atoi(items[1])
+				line := scanner.Text()
+				profileItem, err := c.ParseProfileLine(line)
 				if err != nil {
-					return err
+					return fmt.Errorf("cannot parse profile line %q: %+v", line, err)
 				}
-				visited, err := strconv.Atoi(items[2])
-				if err != nil {
-					return err
+				// only sum total statements when reading the first cover profile
+				if profilesRead == 1 {
+					c.NumberStatements += profileItem.Statements
 				}
-				if visited < 1 {
+				if !profileItem.Visited {
 					continue
 				}
-				_, exists := branchesCount[branch]
+				_, exists := branchesCount[profileItem.Branch]
 				if !exists {
-					branchesStmts[branch] = nrStmt
+					branchesStmts[profileItem.Branch] = profileItem.Statements
 				}
-				branchesCount[branch]++
-				c.Tests[testName] = append(c.Tests[testName], branch)
+				branchesCount[profileItem.Branch]++
+				c.Tests[testName] = append(c.Tests[testName], profileItem.Branch)
 			}
 			if err := scanner.Err(); err != nil {
 				return err
@@ -91,43 +130,8 @@ func (c *CoverProfile) ParseCoverProfile() error {
 		return err
 	}
 	for branch, times := range branchesCount {
-		if times > 1 {
-			continue
-		}
-		c.UniqueBranches[branch] = branchesStmts[branch]
-	}
-	return nil
-}
-
-func (c *CoverProfile) ParseTotalStatements() error {
-	files, err := os.ReadDir(c.DirPath)
-	if err != nil {
-		return err
-	}
-	for _, file := range files {
-		if filepath.Ext(file.Name()) == ".coverprofile" {
-			f, err := os.Open(c.DirPath + "/" + file.Name())
-			if err != nil {
-				return err
-			}
-			defer f.Close()
-			scanner := bufio.NewScanner(f)
-			// dicard first line `mode: xxx`
-			scanner.Scan()
-			// line example with headers
-			// identifier          statements visited
-			// xyz/xyz.go:3.24,5.2 1          1
-			for scanner.Scan() {
-				testStmt, err := strconv.Atoi(strings.Fields(scanner.Text())[1])
-				if err != nil {
-					return err
-				}
-				c.NumberStatements += testStmt
-			}
-			if err := scanner.Err(); err != nil {
-				return err
-			}
-			break
+		if times < 2 {
+			c.UniqueBranches[branch] = branchesStmts[branch]
 		}
 	}
 	return nil
@@ -145,10 +149,6 @@ func NewCoverProfile(dirPath string) (*CoverProfile, error) {
 		DirPath:        dirPath,
 		UniqueBranches: map[string]int{},
 		Tests:          map[string][]string{},
-	}
-	err = covProf.ParseTotalStatements()
-	if err != nil {
-		return &CoverProfile{}, err
 	}
 	err = covProf.ParseCoverProfile()
 	if err != nil {
