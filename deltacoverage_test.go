bc5d2401579d6da072ae5c9dcfc5de3dbc94d650
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2022-09-30 09:32:50 -0300

    Add table tests for two biggest behaviours

--- deltacoverage_test.go
+++ deltacoverage_test.go
@@ -57,133 +57,173 @@ func TestNewCoverProfile_SetsNonEmptyOutputPath(t *testing.T) {
 }
 
 func TestParse_ReturnsExpectedUniqueBranchesGivenOutputPath(t *testing.T) {
-	t.Parallel()
-	c, err := deltacoverage.NewCoverProfile(t.TempDir())
-	if err != nil {
-		t.Fatal(err)
+	testCases := []struct {
+		desc       string
+		outputPath string
+		want       map[string]int
+	}{
+		{
+			desc:       "With simple coverprofiles",
+			outputPath: "testdata/simplecoverprofiles",
+			want:       map[string]int{"a/a.go:7.30,9.2": 1},
+		},
+		{
+			desc:       "With complex coverprofiles",
+			outputPath: "testdata/coverprofiles",
+			want: map[string]int{
+				"github.com/broadwing/deltacoverage/deltacoverage.go:104.4,106.23":  3,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:106.23,109.19": 3,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:113.26,115.6":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:113.5,113.26":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:116.29,117.14": 1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:116.5,116.29":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:119.5,120.12":  2,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:120.12,122.6":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:123.5,124.70":  2,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:126.4,126.40":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:131.43,132.16": 1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:132.16,134.4":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:141.16,143.3":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:144.19,146.3":  1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:35.75,36.38":   1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:36.38,38.3":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:39.2,45.16":    5,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:48.2,50.16":    3,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:53.2,53.22":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:53.22,55.3":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:56.2,56.22":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:62.23,64.3":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:65.2,65.37":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:65.37,67.26":   2,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:67.26,69.14":   2,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:69.14,70.13":   1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:72.4,72.66":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:74.3,74.73":    1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:76.2,77.35":    2,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:96.29,97.51":   1,
+				"github.com/broadwing/deltacoverage/deltacoverage.go:97.51,101.18":  4,
+			},
+		},
 	}
-	c.OutputPath = "testdata/coverprofiles"
-	err = c.Parse()
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := map[string]int{
-		"github.com/broadwing/deltacoverage/deltacoverage.go:104.4,106.23":  3,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:106.23,109.19": 3,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:113.26,115.6":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:113.5,113.26":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:116.29,117.14": 1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:116.5,116.29":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:119.5,120.12":  2,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:120.12,122.6":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:123.5,124.70":  2,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:126.4,126.40":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:131.43,132.16": 1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:132.16,134.4":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:141.16,143.3":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:144.19,146.3":  1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:35.75,36.38":   1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:36.38,38.3":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:39.2,45.16":    5,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:48.2,50.16":    3,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:53.2,53.22":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:53.22,55.3":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:56.2,56.22":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:62.23,64.3":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:65.2,65.37":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:65.37,67.26":   2,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:67.26,69.14":   2,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:69.14,70.13":   1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:72.4,72.66":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:74.3,74.73":    1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:76.2,77.35":    2,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:96.29,97.51":   1,
-		"github.com/broadwing/deltacoverage/deltacoverage.go:97.51,101.18":  4,
-	}
-	got := c.UniqueBranches
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			c, err := deltacoverage.NewCoverProfile(t.TempDir())
+			if err != nil {
+				t.Fatal(err)
+			}
+			c.OutputPath = tC.outputPath
+			err = c.Parse()
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := c.UniqueBranches
+			if !cmp.Equal(tC.want, got) {
+				t.Error(cmp.Diff(tC.want, got))
+			}
+		})
 	}
 }
 
 func TestParse_ReturnsExpectedTestsBranchesGivenOutputPath(t *testing.T) {
-	t.Parallel()
-	c, err := deltacoverage.NewCoverProfile(t.TempDir())
-	if err != nil {
-		t.Fatal(err)
+	testCases := []struct {
+		desc       string
+		outputPath string
+		want       map[string][]string
+	}{
+		{
+			desc:       "With simple coverprofiles",
+			outputPath: "testdata/simplecoverprofiles",
+			want: map[string][]string{
+				"TestSubstractTwoMinusTwo": {"a/a.go:7.30,9.2"},
+				"TestSumOnePlusOne":        {"a/a.go:3.24,5.2"},
+				"TestSumTwoPlusTwo":        {"a/a.go:3.24,5.2"},
+			},
+		},
+		{
+			desc:       "With complex coverprofiles",
+			outputPath: "testdata/coverprofiles",
+			want: map[string][]string{
+				"TestNewCoverProfile_ErrorsIfPathDoesNotExist": {
+					"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:141.16,143.3",
+				},
+				"TestNewCoverProfile_ErrorsIfPathIsNotDirectory": {
+					"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:144.2,144.19",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:144.19,146.3",
+				},
+				"TestParseCoverProfile_ReturnsExpectedCoverProfileGivenCoverProfileDirectory": {
+					"github.com/broadwing/deltacoverage/deltacoverage.go:35.75,36.38",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:39.2,45.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:48.2,50.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:53.2,53.22",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:56.2,56.22",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:36.38,38.3",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:53.22,55.3",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:88.50,93.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:96.2,96.29",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:131.2,131.43",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:136.2,136.12",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:96.29,97.51",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:97.51,101.18",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:104.4,106.23",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:126.4,126.40",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:106.23,109.19",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:113.5,113.26",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:116.5,116.29",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:119.5,120.12",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:123.5,124.70",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:113.26,115.6",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:116.29,117.14",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:120.12,122.6",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:131.43,132.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:132.16,134.4",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:144.2,144.19",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:147.2,153.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:156.2,156.21",
+				},
+				"TestPrintDeltaCoverage_PrintsCorrectPercentDeltaCoverageGivenCoverProfile": {
+					"github.com/broadwing/deltacoverage/deltacoverage.go:60.39,62.23",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:65.2,65.37",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:76.2,77.35",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:65.37,67.26",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:74.3,74.73",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:67.26,69.14",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:72.4,72.66",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:69.14,70.13",
+				},
+				"TestPrintDeltaCoverage_PrintsNoTestsFoundGivenDirectoryWithNoCoverProfile": {
+					"github.com/broadwing/deltacoverage/deltacoverage.go:60.39,62.23",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:62.23,64.3",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:88.50,93.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:96.2,96.29",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:131.2,131.43",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:136.2,136.12",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:144.2,144.19",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:147.2,153.16",
+					"github.com/broadwing/deltacoverage/deltacoverage.go:156.2,156.21",
+				},
+			},
+		},
 	}
-	c.OutputPath = "testdata/coverprofiles"
-	err = c.Parse()
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := map[string][]string{
-		"TestNewCoverProfile_ErrorsIfPathDoesNotExist": {
-			"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:141.16,143.3",
-		},
-		"TestNewCoverProfile_ErrorsIfPathIsNotDirectory": {
-			"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:144.2,144.19",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:144.19,146.3",
-		},
-		"TestParseCoverProfile_ReturnsExpectedCoverProfileGivenCoverProfileDirectory": {
-			"github.com/broadwing/deltacoverage/deltacoverage.go:35.75,36.38",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:39.2,45.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:48.2,50.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:53.2,53.22",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:56.2,56.22",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:36.38,38.3",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:53.22,55.3",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:88.50,93.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:96.2,96.29",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:131.2,131.43",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:136.2,136.12",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:96.29,97.51",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:97.51,101.18",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:104.4,106.23",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:126.4,126.40",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:106.23,109.19",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:113.5,113.26",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:116.5,116.29",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:119.5,120.12",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:123.5,124.70",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:113.26,115.6",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:116.29,117.14",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:120.12,122.6",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:131.43,132.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:132.16,134.4",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:144.2,144.19",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:147.2,153.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:156.2,156.21",
-		},
-		"TestPrintDeltaCoverage_PrintsCorrectPercentDeltaCoverageGivenCoverProfile": {
-			"github.com/broadwing/deltacoverage/deltacoverage.go:60.39,62.23",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:65.2,65.37",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:76.2,77.35",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:65.37,67.26",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:74.3,74.73",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:67.26,69.14",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:72.4,72.66",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:69.14,70.13",
-		},
-		"TestPrintDeltaCoverage_PrintsNoTestsFoundGivenDirectoryWithNoCoverProfile": {
-			"github.com/broadwing/deltacoverage/deltacoverage.go:60.39,62.23",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:62.23,64.3",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:88.50,93.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:96.2,96.29",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:131.2,131.43",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:136.2,136.12",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:139.61,141.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:144.2,144.19",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:147.2,153.16",
-			"github.com/broadwing/deltacoverage/deltacoverage.go:156.2,156.21",
-		},
-	}
-	got := c.TestsBranches
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			c, err := deltacoverage.NewCoverProfile(t.TempDir())
+			if err != nil {
+				t.Fatal(err)
+			}
+			c.OutputPath = tC.outputPath
+			err = c.Parse()
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := c.TestsBranches
+			if !cmp.Equal(tC.want, got) {
+				t.Error(cmp.Diff(tC.want, got))
+			}
+		})
 	}
 }
 
